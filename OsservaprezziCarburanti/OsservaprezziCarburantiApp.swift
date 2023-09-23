388f3e27a7db7aa171b553fe86f2fe326c43720e
Alex Sandri <me@alexsandri.com>
2023-09-23 09:38:58 +0200

    Initial Commit

---
+++ OsservaprezziCarburanti/OsservaprezziCarburantiApp.swift
@@ -0,0 +1,17 @@
+//
+//  OsservaprezziCarburantiApp.swift
+//  OsservaprezziCarburanti
+//
+//  Created by Alex Sandri on 23/09/23.
+//
+
+import SwiftUI
+
+@main
+struct OsservaprezziCarburantiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
